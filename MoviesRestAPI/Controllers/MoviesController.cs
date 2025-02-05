1e325d747011eb8913434de0c39bebad1e84f2de
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-02-05 19:30:14 +0000

    Set up ORM and db connection. Added docker compose for postgres

---
+++ MoviesRestAPI/Controllers/MoviesController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesRestAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MoviesController : ControllerBase {
+    
+}
--- MoviesRestAPI/Program.cs
+++ MoviesRestAPI/Program.cs
@@ -1,12 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesRestAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Ensure the database is created when the application starts
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();  // This will create the tables based on your models
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
