a67d17ff1f4b86233f71832a849994f1adc112fe
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-02-05 20:20:09 +0000

    Removed warnings about required fields

--- MoviesRestAPI/Models/Genre.cs
+++ MoviesRestAPI/Models/Genre.cs
@@ -9,8 +9,8 @@ public class Genre
 
     [Required]
     [MaxLength(255)]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     // (1 -> Many relationship)
-    public ICollection<Movie> Movies { get; set; }
+    public required ICollection<Movie> Movies { get; set; }
 }
--- MoviesRestAPI/Models/Movie.cs
+++ MoviesRestAPI/Models/Movie.cs
@@ -9,16 +9,19 @@ public class Movie
 
     [Required]
     [MaxLength(255)]
-    public string Title { get; set; }
+    public required string Title { get; set; }
 
-    public int ReleaseYear { get; set; }
+    
+    [Range(1888, 2025, ErrorMessage = "Release year must be between 1888 and 2025.")]
+    [Required]
+    public required int ReleaseYear { get; set; }
 
     [MaxLength(1000)]
-    public string Description { get; set; }
+    public required string Description { get; set; }
 
     // Foreign Key
     public int GenreId { get; set; }
 
     // Navigation Property
-    public Genre Genre { get; set; }
+    public required Genre Genre { get; set; }
 }
